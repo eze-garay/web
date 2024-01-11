7f60f7167f87369d01517ac62ee2ccbbc319a07d
eze <ezequielgaray37@gmail.com>
2024-01-11 12:04:46 -0300

    banner ok

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React from 'react';
+import './Blog.css'
+import banner from './assest/banner.jpg'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='conteiner'>
+      <header className='conteinerBanner'>
+        <div className='Banner'> 
+          <div className='BannerTex'>
+            <h1 className='BannerTexTitle'>
+              Jackery
+
+            </h1>
+            <h2 className='BannerTexSubTitle'>
+              GENERADORES SOLARES
+
+            </h2>
+            <p className='BannerTexBody' >
+            Una solución que utiliza energía verde y renovable para exploración al aire libre y respaldo de emergencia
+
+            </p>
+
+          </div>
+          <div className='BannerImg'>
+            <img src={banner} alt='Banner' className='img' ></img>
+
+          </div>
+        </div>
+
+      </header>
+
+      <section className='conteinerCards'>
+
+      </section>
+
+      <footer className='conteinerFooter'>
+
+      </footer>
+    </div>
   )
 }
 
