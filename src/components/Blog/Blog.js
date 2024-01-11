349cdfea891da9630ddd83ec50350229f9f0e708
eze <ezequielgaray37@gmail.com>
2024-01-11 14:17:57 -0300

    footer ok

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Blog.css'
 import banner from './assest/banner.jpg'
+import { TiSocialFacebook } from "react-icons/ti";
+import { TiSocialTwitter } from "react-icons/ti";
+import { SlSocialInstagram } from "react-icons/sl";
 
 const Blog = () => {
   return (
@@ -35,6 +38,32 @@ const Blog = () => {
       </section>
 
       <footer className='conteinerFooter'>
+      <p className='footerSocialTitle'>Jackery</p>
+      <section className='footerSocial'>
+        <div className='footerLinks'>
+            <a href="" className='Links' >
+              <TiSocialFacebook/>
+            </a>
+            <a href="" className='Links'>
+              <TiSocialTwitter/>
+            </a>
+            <a href="" className='Links'>
+            <SlSocialInstagram/>
+            </a>
+        </div>
+      </section>
+      <section className='footerTex'>
+        <div className='footerTex1'>
+          <p>Contactenos : 0351000000</p>
+        </div>
+        <div className='footerTex1'>
+        <p>Calle Falsa 123</p>
+        </div>
+        <div className='footerTex1'>
+        <p>consultas@jackery.com</p>
+        </div>
+      </section>
+      <p className='footerTex2'>@2024 Jackery Inc. All rights reserved</p>
 
       </footer>
     </div>
