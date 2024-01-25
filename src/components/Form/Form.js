9ebeca31b1def67a72527f335f93c85c28bee0e6
eze <ezequielgaray37@gmail.com>
2024-01-25 14:06:58 -0300

    form completo

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,48 +1,66 @@
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form'
 import './Form.css'
 
  const Form = () => {
-        const [nombre, setNombre] = useState('');
-        const [email, setEmail] = useState('');
-        const [mensaje, setMensaje] = useState('');
-        const [imagen, setImagen] = useState(null);
+        const { register, handleSubmit } = useForm ()
+
+      
+
         const [recibirInformacion, setRecibirInformacion] = useState(false);
       
-        const handleSubmit = (e) => {
-          e.preventDefault();
-          // Aquí puedes manejar la lógica para enviar los datos del formulario
-        };
+        const enviar = (data) => {
+            console.log(data);
+          
+            const recibirNovedades = data.recibirInformacion ? 'Sí' : 'No';
+            console.log(`¿Recibir novedades? ${recibirNovedades}`);
+          };
+      
+   
       
         return (
             <div>
-                <form className='Form' onSubmit={handleSubmit}>
-                
+                <form  onSubmit={handleSubmit(enviar)}>
+                <div className='Form'>
                 <div className="FormText">
-                    <label className="FormTextTitle" htmlFor="email">Email*</label>
-                    <input className="FormTextCont" type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                        <label className="FormTextTitle" htmlFor="email">Email*</label>
+                        <input className="FormTextCont" type="email"{...register("email")} required />
 
-                    <label className="FormTextTitle" htmlFor="nombre">Nombre*</label>
-                    <input className="FormTextCont" type="text" id="nombre" name="nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} required />
+                        <label className="FormTextTitle" htmlFor="nombre">Nombre*</label>
+                        <input className="FormTextCont" type="text" {...register("nombre")}required/>
 
-                    <label className="FormTextTitle" htmlFor="mensaje">Mensaje*</label>
-                    <textarea className="FormTextCont" id="mensaje" name="mensaje" value={mensaje} onChange={(e) => setMensaje(e.target.value)} rows="6" required></textarea>
+                        <label className="FormTextTitle" htmlFor="mensaje">Mensaje*</label>
+                        <textarea className="FormTextCont" {...register("mensaje")}  rows="6" required></textarea>
+                    </div>
+                    <div className="FormImg">
+                    <label className="FileButton" htmlFor="imagen">
+                        +
+                    </label>
+                    <input
+                        className="FileInput"
+                        type="file"
+                        id="imagen"
+                        name="imagen"
+                        {...register("imagen")}
+                        accept="image/*"
+                        />
+                    <label className="FormImgTitle" htmlFor="imagen">
+                        Adjuntar archivo (10MG máx.)
+                    </label>
+                    </div>
                 </div>
-
-                <div className="FormImg">
-                    <input type="file" id="imagen" name="imagen" onChange={(e) => setImagen(e.target.files[0])} accept="image/*" />
-                    <label className="FormImgTitle" htmlFor="imagen">Adjuntar archivo (10MG máx.) </label>
-                </div>
-                </form>
- 
                 <div id="opcion" className="FormButton">
                 <label className="FormTextButton">
-                  <input  type="checkbox" checked={recibirInformacion} onChange={() => setRecibirInformacion(!recibirInformacion)} />
+                  <input  type="checkbox" {...register("recibirInformacion")} checked={recibirInformacion} onChange={() => setRecibirInformacion(!recibirInformacion)} />
                   Quiero recibir novedades de Jackery en mi correo.
                 </label>
                 </div>
                 <div className="FormButton">
                 <button className="myButton" type="submit">Enviar</button>
                 </div>
+            
+                </form>
+
             </div>
 
 
