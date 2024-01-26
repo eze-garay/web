2d77b274557dfb2b78a2e5851b48c43c6bef80f7
eze <ezequielgaray37@gmail.com>
2024-01-26 13:44:08 -0300

    nav ok

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,6 +2,9 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from './assest/Logo jackery.svg';
+import icon1 from './assest/iconos redes-01.svg'
+import icon2 from './assest/iconos redes-02.svg'
+import icon3 from './assest/iconos redes-03.svg'
 import { IoMenu } from 'react-icons/io5';
 import { IoClose } from 'react-icons/io5';
 
@@ -35,32 +38,54 @@ const NavBar = () => {
     { id: 3, link: "BLOG" },
     { id: 4, link: "CONTACTO" },
   ];
+  const icons = [
+    { id: 1, icon: icon1, link: '/ruta-icono-1' },
+    { id: 2, icon: icon2, link: '/ruta-icono-2' },
+    { id: 3, icon: icon3, link: '/ruta-icono-3' },
+  ];
 
   return (
-    <div className={!navBarOpen ? 'navBar' : 'navOpen'}>
-      {!navBarOpen && <img src={Logo} alt="Logo" className='logo' />}
-      {!navBarOpen && windowDimension.with < 800 ? (
-        <IoMenu size={50} onClick={() => setNavBarOpen(!navBarOpen)} />
-      ) : (
-        windowDimension.with < 800 && (
-          <IoClose size={25} onClick={() => setNavBarOpen(!navBarOpen)} />
-        )
-      )}
-      {navBarOpen || windowDimension.with > 800 ? (
-        <ul className='linksConteiner'>
-          {links.map((x) => (
-            <div key={x.id}>
-              <Link
-                to={x.link}
-                onClick={() => setNavBarOpen(false)}
-                className='links'
-              >
-                {x.link}
-              </Link>
-            </div>
-          ))}
-        </ul>
-      ) : null}
+    <div className="navContainer">
+      <div className={!navBarOpen ? 'navBar' : 'navOpen'}>
+        <div className="logoContainer">
+          {!navBarOpen && <img src={Logo} alt="Logo" className='logo' />}
+        </div>
+        <div className="linksContainer">
+          {navBarOpen || windowDimension.with > 800 ? (
+            <ul className='linksConteiner'>
+              {links.map((x) => (
+                <li key={x.id}>
+                  <Link
+                    to={x.link}
+                    onClick={() => setNavBarOpen(false)}
+                    className='links'
+                  >
+                    {x.link}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : null}
+        </div>
+        <div className="iconsContainer">
+          {!navBarOpen && windowDimension.with > 800 && (
+            <>
+              {icons.map((x) => (
+                <Link key={x.id} to={x.link} className="iconLink">
+                  <img src={x.icon} alt={`Icon ${x.id}`} className="icon" />
+                </Link>
+              ))}
+            </>
+          )}
+          {!navBarOpen && windowDimension.with <= 800 ? (
+            <IoMenu size={50} onClick={() => setNavBarOpen(!navBarOpen)} />
+          ) : (
+            windowDimension.with < 800 && (
+              <IoClose size={25} onClick={() => setNavBarOpen(!navBarOpen)} />
+            )
+          )}
+        </div>
+      </div>
     </div>
   );
 };
